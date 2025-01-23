1edc8c0977c5b44878a5ac41826e2223606cd0a4
Oleh Kuzo <olegkuzo23@gmail.com>
2025-01-23 17:00:52 +0200

    added terraform

---
+++ infra/deploy/variables.tf
@@ -0,0 +1,14 @@
+variable "prfix" {
+  description = "Prefix for resources in AWS"
+  default     = "raa"
+}
+
+variable "project" {
+  description = "Project name for tagging resources"
+  default     = "recipe-app-api"
+}
+
+variable "contact" {
+  description = "Contant email for tagging resources"
+  default     = "olegkuzo23@gmail.com"
+}
