65459b83358107c66e1984406b2b99ec30ebbf44
Oleh Kuzo <olegkuzo23@gmail.com>
2025-01-27 19:11:31 +0200

    fmt

--- infra/setup/iam.tf
+++ infra/setup/iam.tf
@@ -33,7 +33,7 @@ data "aws_iam_policy_document" "tf_backend" {
       "s3:GetObject",
       "s3:PutObject",
       "s3:DeleteObject",
-      "s3:HeadObject"  # Added HeadObject permission
+      "s3:HeadObject" # Added HeadObject permission
     ]
     resources = [
       "arn:aws:s3:::${var.tf_state_bucket}/tf-state-deploy/*",
