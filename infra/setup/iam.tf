df368f892a6e0de963872fce4173b3a111a1eaa9
Oleh Kuzo <olegkuzo23@gmail.com>
2025-01-28 21:19:59 +0200

    rds fix

--- infra/setup/iam.tf
+++ infra/setup/iam.tf
@@ -158,6 +158,7 @@ data "aws_iam_policy_document" "rds" {
   statement {
     effect = "Allow"
     actions = [
+      "rds:AddTagsToResource",
       "rds:DescribeDBSubnetGroups",
       "rds:DescribeDBInstances",
       "rds:CreateDBSubnetGroup",
